d3de6b1bf6456c20c1566bd6f976fbd08c7e0ea1
IceHe.xyz <icehe@silverhand.io>
2022-07-20 18:42:10 +0800

    test(test): set up a social connector

--- packages/integration-tests/src/connector-api.ts
+++ packages/integration-tests/src/connector-api.ts
@@ -4,3 +4,21 @@ export const listConnectors = async () => authedAdminApi.get('connectors');
 
 export const getConnectorDetails = async (connectorId: string) =>
   authedAdminApi.get(`connectors/${connectorId}`);
+
+export const updateConnectorConfig = async (connectorId: string, config: Record<string, unknown>) =>
+  authedAdminApi.patch({
+    url: `connectors/${connectorId}`,
+    json: { config },
+    headers: {
+      'development-user-id': 'integration-test-admin-user',
+    },
+  });
+
+export const enableConnector = async (connectorId: string) =>
+  authedAdminApi.patch({
+    url: `connectors/${connectorId}/enabled`,
+    json: { enabled: true },
+    headers: {
+      'development-user-id': 'integration-test-admin-user',
+    },
+  });
--- packages/integration-tests/tests/connectors.test.ts
+++ packages/integration-tests/tests/connectors.test.ts
@@ -1,6 +1,13 @@
 import { ConnectorDto } from '@logto/schemas';
 
-import { getConnectorDetails, listConnectors } from '@/connector-api';
+import {
+  enableConnector,
+  getConnectorDetails,
+  listConnectors,
+  updateConnectorConfig,
+} from '@/connector-api';
+
+const facebookConnectorId = 'facebook-universal';
 
 describe('list connectors and get details before setting up', () => {
   it('should succeed to get all connectors and they are all disabled', async () => {
@@ -15,12 +22,48 @@ describe('list connectors and get details before setting up', () => {
   });
 
   it('should succeed to get connector details', async () => {
-    const response = await getConnectorDetails('facebook-universal');
+    const response = await getConnectorDetails(facebookConnectorId);
     expect(response).toHaveProperty('statusCode', 200);
     expect(() => {
       const connectorDto = JSON.parse(response.body) as ConnectorDto;
       expect(connectorDto.enabled).toBeFalsy();
       expect(connectorDto.config).toEqual({});
     }).not.toThrow();
+  });
+});
+
+describe('set up connectors', () => {
+  describe('set up social connectors', () => {
+    it('should succeed to update the social connector config and enable it', async () => {
+      const facebookConfig = {
+        clientId: 'application_foo',
+        clientSecret: 'secret_bar',
+      };
+
+      const updatedConfigResponse = await updateConnectorConfig(
+        facebookConnectorId,
+        facebookConfig
+      );
+      expect(updatedConfigResponse).toHaveProperty('statusCode', 200);
+      expect(() => {
+        const updatedConnectorDto = JSON.parse(updatedConfigResponse.body) as ConnectorDto;
+        expect(updatedConnectorDto.config).toEqual(facebookConfig);
+      }).not.toThrow();
+
+      const enabledResponse = await enableConnector(facebookConnectorId);
+      expect(enabledResponse).toHaveProperty('statusCode', 200);
+      expect(() => {
+        const enabledConnectorDto = JSON.parse(enabledResponse.body) as ConnectorDto;
+        expect(enabledConnectorDto.enabled).toBeTruthy();
+      }).not.toThrow();
+    });
+  });
+
+  describe('set up sms connectors', () => {
+    // Next up
+  });
+
+  describe('set up email connectors', () => {
+    // Next up
   });
 });
